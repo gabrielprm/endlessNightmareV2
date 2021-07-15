ac9636ae121756fb9144d03ea62e748a62e67eac
Lucas Alexandre Amorim Leoncio <77064352+Lucas-AlexandreL@users.noreply.github.com>
2021-07-15 19:09:05 -0300

    Incrementacao do calculo de dificuldade

--- endlessNightmare/Systems/DifficultyIncrement.swift
+++ endlessNightmare/Systems/DifficultyIncrement.swift
@@ -5,4 +5,30 @@
 //  Created by Gabriel do Prado Moreira on 15/07/21.
 //
 
-import Foundation
+import SpriteKit
+
+
+class DifficultyIncrement{
+    
+    @propertyWrapper
+    private struct coefficientOfVelocityCalculator {
+        private var coefficient = 0.0
+        var wrappedValue: Double {
+            get { return coefficient }
+            set {
+                coefficient = newValue * (log2(newValue) / 2)
+            }
+        }
+    }
+    
+    @coefficientOfVelocityCalculator private var difficultyCounter:Double
+    
+    func speedProgression(xMap:CGFloat) -> Double{
+        
+        let x = Double(xMap)
+        difficultyCounter = x
+        
+        return difficultyCounter
+    }
+    
+}
