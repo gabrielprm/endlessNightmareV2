14cf975b082f328943ec29ea8568825c6c05279b
gabrielprm <60402896+gabrielprm@users.noreply.github.com>
2021-07-15 15:14:11 -0300

    MapUpdateFunc

--- endlessNightmare/Systems/MapGenerator.swift
+++ endlessNightmare/Systems/MapGenerator.swift
@@ -9,27 +9,43 @@ import SpriteKit
 import GameplayKit
 
 public class Map: SKNode{
-    var mapa: SKSpriteNode! = nil
+    var map: SKSpriteNode! = nil
+    let initialXPosition:CGFloat = 1310
+    let initialYPosition:CGFloat = 755
+    let movXCoefficient: CGFloat = -1
+    let movYCoefficient: CGFloat = -0.5773502691896257
     
     init(imageName: String, position: CGPoint, zPosition: CGFloat){
         
         super.init()
         
-        mapa = SKSpriteNode(texture: SKTexture(imageNamed: imageName))
-        mapa.position = position
-        mapa.zPosition = zPosition
+        map = SKSpriteNode(texture: SKTexture(imageNamed: imageName))
+        map.position = position
+        map.zPosition = zPosition
     }
     
     init(imageName: String, zPosition: CGFloat){
         
         super.init()
         
-        mapa = SKSpriteNode(texture: SKTexture(imageNamed: imageName))
-        mapa.zPosition = zPosition
+        map = SKSpriteNode(texture: SKTexture(imageNamed: imageName))
+        map.zPosition = zPosition
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateMap(map: SKSpriteNode, map2: SKSpriteNode){
+      
+        map.run(SKAction.moveBy(x: movXCoefficient, y: movYCoefficient, duration: 0.1))
+
+        if (map.position.x < -1310) {
+            map.position = map2.position
+            map2.position = CGPoint(x: initialXPosition, y: initialYPosition)
+        }
+       
+        map2.run(SKAction.moveBy(x: movXCoefficient, y: movYCoefficient, duration: 0.1))
     }
     
 }
