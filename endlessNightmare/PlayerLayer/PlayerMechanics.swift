0ad528a12115a007bd974bb20e27dadba1a9d5cf
gabrielprm <60402896+gabrielprm@users.noreply.github.com>
2021-07-16 14:39:06 -0300

    Commit

--- endlessNightmare/PlayerLayer/PlayerMechanics.swift
+++ endlessNightmare/PlayerLayer/PlayerMechanics.swift
@@ -12,12 +12,10 @@ public class PlayerMechanics {
     static func moveDown(_ character: SKNode){
         switch characterGenerator.rowPosition {
             case 1:
-                let action = SKAction.move(to: NodePosition.nodePosition(row: 2), duration: 0.1)
-                character.run(action)
+                character.position = NodePosition.nodePosition(row: 2)
                 characterGenerator.rowPosition = 2
             case 2:
-                let action = SKAction.move(to: NodePosition.nodePosition(row: 3), duration: 0.1)
-                character.run(action)
+                character.position = NodePosition.nodePosition(row: 3)
                 characterGenerator.rowPosition = 3
             default:
                 break
@@ -27,12 +25,10 @@ public class PlayerMechanics {
     static func moveUp(_ character: SKNode){
         switch characterGenerator.rowPosition {
             case 2:
-                let action = SKAction.move(to: NodePosition.nodePosition(row: 1), duration: 0.1)
-                character.run(action)
+                character.position = NodePosition.nodePosition(row: 1)
                 characterGenerator.rowPosition = 1
             case 3:
-                let action = SKAction.move(to: NodePosition.nodePosition(row: 2), duration: 0.1)
-                character.run(action)
+                character.position = NodePosition.nodePosition(row: 2)
                 characterGenerator.rowPosition = 2
             default:
                 break
