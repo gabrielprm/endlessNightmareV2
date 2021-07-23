6499c9f95806570d00d81adf2fe617fd0c4b1466
Johnny Camacho <joh_camacho@outlook.com>
2021-07-22 22:24:09 -0300

    Verificação da última posição dos inimigos gerados.

--- endlessNightmare/EnemyLayer/EnemyManager.swift
+++ endlessNightmare/EnemyLayer/EnemyManager.swift
@@ -9,13 +9,18 @@ import SpriteKit
 
 struct EnemyManager {
     
-
     static let enemyMasterNode = SKNode()
+    static var lastPosition = 0
     
     static func enemyBorn() {
         let node = EnemyGenerator()
+        var random = Int.random(in: 1...3)
         
-        switch Int.random(in: 1...3) {
+        while random == lastPosition {
+            random = Int.random(in: 1...3)
+        }
+        
+        switch random {
         case 1:
             let diferenca = CGPoint(x: 580 - enemyMasterNode.position.x, y: 340 - enemyMasterNode.position.y)
             
@@ -38,20 +43,16 @@ struct EnemyManager {
             break
         }
         
+        lastPosition = random
         enemyMasterNode.addChild(node)
     }
     
-
-    static func enemyDie(node: SKSpriteNode){
+    static func enemyDie(node: SKSpriteNode) {
        
     }
-    
-    //static func move(masterNode: SKNode){
-    //   masterNode.run(SKAction.moveBy(x: MapData.movXCoefficient, y: MapData.movYCoefficient, duration: 1))
 
-    static func move(count:CGFloat ) {
+    static func move(count: CGFloat) {
         enemyMasterNode.run(SKAction.moveBy(x: MapData.movXCoefficient * count, y: MapData.movYCoefficient * count, duration: 1))
-
     }
     
 }
