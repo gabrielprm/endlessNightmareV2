07819990d47c03572161d45e448c4a79d4cf2694
Johnny Camacho <joh_camacho@outlook.com>
2021-07-23 17:17:46 -0300

    Adicionando novos backgrounds

--- endlessNightmare/BackgroundLayer/Background.swift
+++ endlessNightmare/BackgroundLayer/Background.swift
@@ -10,20 +10,54 @@ import SpriteKit
 
 class Background: SKSpriteNode {
     
-    init(position: CGPoint) {
-        let background = SKTexture(imageNamed: "background")
-        
-        super.init(texture: background, color: .clear, size: background.size())
+    let fundo1 = SKTexture(imageNamed: "FundoTeste1")
+    let fundo2 = SKTexture(imageNamed: "FundoTeste2")
+    let fundo3 = SKTexture(imageNamed: "FundoTeste3")
+    let fundo4 = SKTexture(imageNamed: "FundoTeste4")
     
+    var textures: [SKTexture] {
+        return [fundo1, fundo2, fundo3, fundo4]
+    }
+    
+    init(position: CGPoint) {
+//        let background = SKTexture(imageNamed: "background")
+        
+        super.init(texture: fundo1, color: .clear, size: fundo1.size())
+        
         self.setScale(1.5)
         self.position = position
         self.zPosition = -1
+        
+        let animation = SKAction.animate(with: textures, timePerFrame: 1)
+        let runAnimation = SKAction.repeatForever(animation)
+        
+        run(runAnimation)
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         self.zPosition = -1
+        
+//        let firstBackground = SKSpriteNode(texture: fundo1, size: size)
+        let secondBackground = SKSpriteNode(texture: fundo2, size: size)
+        let thirdBackground = SKSpriteNode(texture: fundo3, size: size)
+        let fourthBackground = SKSpriteNode(texture: fundo4, size: size)
+        
+//        addChild(firstBackground)
+        addChild(secondBackground)
+        addChild(thirdBackground)
+        addChild(fourthBackground)
+        
+        let fadeOut = SKAction.fadeOut(withDuration: 5)
+        let fadeIn = SKAction.fadeIn(withDuration: 5)
+        
+//        firstBackground.run(fadeOut)
+        
+        run(fadeOut)
+        secondBackground.run(fadeIn)
+        
+        print("teste")
     }
     
 }
