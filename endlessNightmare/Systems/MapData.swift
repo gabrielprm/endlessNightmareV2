608f67dc0ffb124e7dfd7219a019f23f2242ae73
Caliope-Victor <72174923+Caliope-Victor@users.noreply.github.com>
2021-07-31 11:14:41 -0300

    Ajustando a dificuldade, e adicionando os brilhos em parallax no fundo

---
+++ endlessNightmare/Systems/MapData.swift
@@ -0,0 +1,31 @@
+//
+//  MapData.swift
+//  endlessNightmare
+//
+//  Created by Gabriel do Prado Moreira on 15/07/21.
+//
+
+import Foundation
+import SpriteKit
+
+//Chao Dimensao : 1571 × 895
+
+public struct MapData {
+    static let initialXPositionSecondMap: CGFloat = 730.75
+    static let initialYPositionSecondMap: CGFloat = 422.63
+    static let movXCoefficient: CGFloat = -1
+    static let movYCoefficient: CGFloat = -0.5773502691896257
+    
+    static let initialXpositionFirstSparkleUp = 0
+    static let initialYpositionFirstSparkleUp = 70
+    
+    static let initialXpositionSecondSparkleUp = 1150
+    static let initialYpositionSecondSparkleUp = 70
+    
+    static let initialXpositionFirstSparkleDown = -510
+    static let initialYpositionFirstSparkleDown = -283
+    
+    static let initialXpositionSecondSparkleDown = 1150
+    static let initialYpositionSecondSparkleDown = -283
+
+}
